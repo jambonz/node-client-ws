2a60ffdeac54ab51df1a64a288428b65ce7bd933
Dave Horton <daveh@beachdognet.com>
2023-01-21 16:53:24 -0500

    pass req.path in session:new event

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const handleProtocols = (protocols) => {
   if (!protocols.has('ws.jambonz.org')) return false;
   return 'ws.jambonz.org';
 };
+const parseurl = require('parseurl');
 
 const createEndpoint = ({server, port, logger}) => {
   logger = logger || {info: () => {}, error: () => {}, debug: () => {}};
@@ -16,18 +17,16 @@ const createEndpoint = ({server, port, logger}) => {
   const router = new Router();
   const wss = new WebSocketServer({ noServer: true, handleProtocols });
   server.on('upgrade', (req, socket, head) => {
-    console.log('got upgrade request');
+    const parsed = parseurl(req);
     const client = router.route(req);
     if (!client) {
-      console.log('no client found');
+      logger.info('no client found');
       socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
       socket.destroy();
       return;
     }
-    console.log('found client');
     wss.handleUpgrade(req, socket, head, (ws) => {
-      console.log('upgrade complete');
-      client.handle(ws, req);
+      client.handle(ws, parsed.path);
     });
   });
   if (port) server.listen(port);
